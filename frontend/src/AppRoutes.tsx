d2424c00555719520be048784353b3cfc503d184
Ashish <as7220004@gmail.com>
2025-01-25 18:46:53 +0530

    Added: Access token auth and validation in backend for access token

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -1,15 +1,17 @@
 import { Route, Routes, Navigate } from "react-router-dom"
 import Layout from "./layouts/layouts";
 import HomePage from "./pages/HomePage";
+import AuthCallbackPage from "./pages/AuthCallbackPage";
 
 const AppRoutes = () => {
     return (
         <Routes>
-            <Route path="/" element={<Layout><HomePage /></Layout>}></Route>
-            <Route path="/user-profile" element={<span>User</span>}></Route>
-            <Route path="*" element={<Navigate to="/" />}></Route>
+            <Route path="/" element={<Layout><HomePage /></Layout>} />
+            <Route path="/auth-callback" element={<AuthCallbackPage />} />
+            <Route path="/user-profile" element={<span>User</span>} />
+            <Route path="*" element={<Navigate to="/" />} />
         </Routes>
     )
 }
 
-export default AppRoutes;
+export default AppRoutes; 
