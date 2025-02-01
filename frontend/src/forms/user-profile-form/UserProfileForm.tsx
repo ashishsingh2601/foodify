7acf259b8c47af51685fd573b6cadb33914a33f7
Ashish <as7220004@gmail.com>
2025-02-01 16:21:53 +0530

    Completed: User profile page, Added: Pre-population of user data if it exists

--- frontend/src/forms/user-profile-form/UserProfileForm.tsx
+++ frontend/src/forms/user-profile-form/UserProfileForm.tsx
@@ -5,11 +5,13 @@ import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, For
 import { Input } from "@/components/ui/input";
 import LoadingButton from "@/components/LoadingButton";
 import { Button } from "@/components/ui/button";
+import { User } from "@/types";
+import { useEffect } from "react";
 
 const formSchema = z.object({
     email: z.string().optional(),
     name: z.string().min(1, "Name is required"),
-    addressLine1: z.string().min(1, "AddressLine1 is required"),
+    addressLine1: z.string().min(1, "Address Line 1 is required"),
     country: z.string().min(1, "Country is required"),
     city: z.string().min(1, "City is required"),
 });
@@ -19,12 +21,18 @@ type UserFormData = z.infer<typeof formSchema>;
 type Props = {
     onSave: (userProfileData: UserFormData) => void;
     isLoading: boolean;
+    currentUser: User;
 }
 
-const userProfileForm = ({ onSave, isLoading }: Props) => {
+const userProfileForm = ({ onSave, isLoading, currentUser }: Props) => {
     const form = useForm<UserFormData>({
         resolver: zodResolver(formSchema),
+        defaultValues: currentUser,
     });
+
+    useEffect(() => {
+        form.reset(currentUser);
+    }, [currentUser, form])
 
     return (
         <Form {...form}>
--- frontend/src/pages/UserProfilePage.tsx
+++ frontend/src/pages/UserProfilePage.tsx
@@ -1,10 +1,18 @@
-import { useUpdateMyUser } from "@/api/MyUserApi";
+import { useGetMyUser, useUpdateMyUser } from "@/api/MyUserApi";
 import UserProfileForm from "@/forms/user-profile-form/UserProfileForm"
 
 const UserProfilePage = () => {
-    const { updateUser, isLoading } = useUpdateMyUser();
-    
-    return <UserProfileForm onSave={updateUser} isLoading={isLoading} />;
+    const {currentUser, isLoading: isGetLoading} = useGetMyUser();
+    const { updateUser, isLoading:isUpdateLoading} = useUpdateMyUser();
+
+    if(isGetLoading){
+        return <span className="flex justify-center text-orange-600 font-bold">Loading...</span>;
+    }
+    if(!currentUser){
+        return <span>Unable to load user profile</span>;
+    }
+
+    return <UserProfileForm onSave={updateUser} isLoading={isUpdateLoading} currentUser={currentUser} />;
 }
 
 export default UserProfilePage
