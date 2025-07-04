7887491a2c8f5ed26ffff731eb654e10f7f4f55b
ashishsingh2601 <as7220004@gmail.com>
2025-07-04 23:54:23 +0530

    Cloudinary Setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,10 +3,18 @@ import cors from "cors"
 import "dotenv/config"
 import mongoose from "mongoose"
 import myUserRoute from "./routes/MyUserRoute"
+import { v2 as cloudinary } from "cloudinary"
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => {
     console.log("Connected to DB!");
 })
+
+// cloudinary connection
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const app = express();
 
@@ -32,5 +40,4 @@ app.use("/api/my/user", myUserRoute);
 
 app.listen(process.env.PORT || 6000, () => {
     console.log(`Server stated on: ${process.env.PORT}`);
-})
-
+});
