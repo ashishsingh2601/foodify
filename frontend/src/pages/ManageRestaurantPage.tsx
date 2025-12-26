92cb36c7e07ecd8a815416db52667e9ae47a17cd
ashishsingh2601 <ashish.compile@gmail.com>
2025-12-26 15:09:03 +0530

    Completed: Order status update flow

--- frontend/src/pages/ManageRestaurantPage.tsx
+++ frontend/src/pages/ManageRestaurantPage.tsx
@@ -1,20 +1,48 @@
-import { useCreateMyRestaurant, useGetmyRestaurant, useUpdateMyRestaurant } from "@/api/MyRestaurantApi";
+import {
+  useCreateMyRestaurant,
+  useGetmyRestaurant,
+  useGetMyRestaurantOrders,
+  useUpdateMyRestaurant,
+} from "@/api/MyRestaurantApi";
+import OrderItemCard from "@/components/OrderItemCard";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import ManageRestaurantForm from "@/forms/manage-restaurant-form/ManageRestaurantForm";
 
 const ManageRestaurantPage = () => {
-    const { createRestaurant, isLoading: isCreateLoading } = useCreateMyRestaurant();
-    const { restaurant } = useGetmyRestaurant();
-    const { updateRestaurant, isLoading: isUpdateLoading } = useUpdateMyRestaurant();
+  const { createRestaurant, isLoading: isCreateLoading } =
+    useCreateMyRestaurant();
+  const { restaurant } = useGetmyRestaurant();
+  const { updateRestaurant, isLoading: isUpdateLoading } =
+    useUpdateMyRestaurant();
 
-    const isEditing = !!restaurant;
+  const { orders } = useGetMyRestaurantOrders();
 
-    return ( 
-        <ManageRestaurantForm 
-            onSave={isEditing ? updateRestaurant : createRestaurant} 
-            isLoading={isCreateLoading || isUpdateLoading} 
-            restaurant={restaurant} 
+  const isEditing = !!restaurant;
+
+  return (
+    <Tabs defaultValue="orders">
+      <TabsList>
+        <TabsTrigger value="orders">Orders</TabsTrigger>
+        <TabsTrigger value="manage-restaurant">Manage Restaurant</TabsTrigger>
+      </TabsList>
+      <TabsContent
+        value="orders"
+        className="space-y-5 bg-gray-50 p-10 rounded-lg"
+      >
+        <h2 className="text-2xl font-bold">{orders?.length} active orders</h2>
+        {orders?.map((order) => (
+          <OrderItemCard order={order} />
+        ))}
+      </TabsContent>
+      <TabsContent value="manage-restaurant">
+        <ManageRestaurantForm
+          restaurant={restaurant}
+          onSave={isEditing ? updateRestaurant : createRestaurant}
+          isLoading={isCreateLoading || isUpdateLoading}
         />
-    );
+      </TabsContent>
+    </Tabs>
+  );
 };
 
 export default ManageRestaurantPage;
